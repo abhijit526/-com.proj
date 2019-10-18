4124fa28ec470ce16d2cafb657471669a67a79d9
SATYA <SATYA@DESKTOP-4F4CFAG>
2019-10-18 23:48:24 +0530

    CHeck My Commit 2 With Condition

--- src/com/sr/cfgs/AppTest.java
+++ src/com/sr/cfgs/AppTest.java
@@ -1,7 +1,15 @@
 package com.sr.cfgs;
 
 public class AppTest {
+
 	public static void main(String[] args) {
+		
 		System.out.println("Get details...............");
-	}
+/*		if() {
+			System.out.println("Donu Dog....");
+		}
+		else {
+			System.out.println("Donu Fox.....");
+		*/}
+	
 }
